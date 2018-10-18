d89abb524141fb7cd9c9b6f0ad9b08a19ca4e852
Hyunwoo Nam <hw.nam@ridi.com>
2018-10-18 15:52:59 +0900

    escape * for resource name

--- src/terraflare/main.clj
+++ src/terraflare/main.clj
@@ -1,10 +1,12 @@
-(ns terraflare
+(ns terraflare.main
   (:require [clj-http.client :as client]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.string :as str]))
 
 (def CF-EMAIL "")
 (def CF-TOKEN "")
 (def CF-BASE-URL "https://api.cloudflare.com/client/v4/")
+
 
 (defn- cf-send-api
   ([uri]
@@ -18,12 +20,14 @@
      (-> (:body response)
          (json/read-str :key-fn keyword)))))
 
+
 (defn cf-zone-id [domain]
   (when-let [response (cf-send-api "zones")]
     (->> (:result response)
          (drop-while #(not= (:name %) domain))
          first
          :id)))
+
 
 (defn cf-dns-list [zone-id]
   (let [url (str "zones/" zone-id "/dns_records")]
@@ -37,6 +41,7 @@
               (recur (concat records (:result body)) (inc cur_page)))
             records))))))
 
+
 (defn dissoc-with
   "Dissociate map with predicate preserving metadata"
   [m f]
@@ -46,10 +51,14 @@
                  (dissoc m k)))
              m m))
 
+
 (defn rename-key [m k1 k2]
-  (-> m
-      (assoc k2 (get m k1))
-      (dissoc k1)))
+  (if (not= k1 k2)
+    (-> m
+        (assoc k2 (get m k1))
+        (dissoc k1))
+    m))
+
 
 (defn preprocess-record [record]
   (let [default? (fn [k v]
@@ -62,7 +71,8 @@
         (rename-key :content :value)
         (assoc :domain "${var.domain}"))))
 
-(defn dns->hcl [domain dns-records]
+
+(defn- records->tf [domain dns-records]
   (let [scaffold  {:provider {:cloudflare {}}
                    :variable {:domain {:default domain}}
                    :resource {:cloudflare_record {}}}
@@ -70,7 +80,8 @@
         ; avoid name collision
         gen-name  (fn [m]
                     (-> (str (:type m) "-" (:name m) "-")
-                        (clojure.string/replace "." "_")
+                        (str/replace "." "_")
+                        (str/replace "*" "STAR")
                         (gensym)))
 
         resources (->> dns-records
@@ -82,15 +93,15 @@
                        (map preprocess-record)
 
                        (reduce (fn [m v] (assoc m (gen-name v) v)) {})
-                       )
-        ]
+                       )]
     (assoc-in scaffold [:resource :cloudflare_record] resources)))
 
 
-(defn dns->json [domain out]
+(defn tf-json-from [domain]
   (let [zone-id     (cf-zone-id domain)
         dns-records (cf-dns-list zone-id)]
-    (spit out (json/write-str (dns->hcl domain dns-records)))))
+    (json/write-str (records->tf domain dns-records))))
+
 
 (comment
 
@@ -99,7 +110,7 @@
 
   @(def dns-records (cf-dns-list zone-id))
 
-  @(def resources (dns->hcl domain dns-records))
+  @(def resources (records->tf domain dns-records))
 
   (let [domains ["initialcoms.com"
                  "ridi.com"
@@ -111,7 +122,7 @@
                  "studiod.co.kr"]]
     (doseq [domain domains
             :let [json (str domain ".json")]]
-      (dns->json domain json)
+      (spit json (tf-json-from domain))
 
       (println (str "json2hcl < " json " > " domain ".tf"))
       ))
